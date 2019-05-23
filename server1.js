cf01e7018e19adf4e690e4d6615f5d0d2c738fb2
Layla Jooya <laylajooya@gmail.com>
2019-05-22 19:45:52 -0500

    server4

--- server1.js
+++ server1.js
@@ -1,3 +1,4 @@
+
 // Dependencies
 // ===========================================================
 var express = require("express");
@@ -29,30 +30,29 @@ var characters = [{
 
 // Routes
 // ===========================================================
-
 app.get("/", function(req, res) {
   res.send("Welcome to the Star Wars Page!");
 });
 
-// What does this route do?
+// Displays all characters
 app.get("/api/characters", function(req, res) {
   return res.json(characters);
 });
 
-// What does this route do?
+// Displays a single character, or shows "No character found"
 app.get("/api/characters/:character", function(req, res) {
-  // What does this code do?
+  // Grab the selected parameter
   var chosen = req.params.character;
   console.log(chosen);
 
-  // What does this code do?
+  // Filter to show only the selected character
   for (var i = 0; i < characters.length; i++) {
     if (chosen === characters[i].routeName) {
       return res.json(characters[i]);
     }
   }
 
-  // What does this code do?
+  // Otherwise display "No character found"
   return res.send("No character found");
 });
 
@@ -61,4 +61,3 @@ app.get("/api/characters/:character", function(req, res) {
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
-
